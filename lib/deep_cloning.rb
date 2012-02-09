735560f976c520b1d12c0b15798e579cb84434e9
Velimir Lesikov <velimirles@gmail.com>
2012-02-08 16:50:45 -0800

    Changing functionalaty to dup instead of clone. Removing saveing new duped object

--- lib/deep_cloning.rb
+++ lib/deep_cloning.rb
@@ -7,28 +7,29 @@
 # === Usage:
 # 
 # ==== Cloning a model without an attribute
-#   pirate.clone! :except => :name
+#   pirate.deep_dup! :except => :name
 # 
 # ==== Cloning a model without multiple attributes
-#   pirate.clone! :except => [:name, :nick_name]
+#   pirate.deep_dup! :except => [:name, :nick_name]
+#
 # ==== Cloning one single association
-#   pirate.clone! :include => :mateys
+#   pirate.deep_dup! :include => :mateys
 #
 # ==== Cloning multiple associations
-#   pirate.clone! :include => [:mateys, :treasures]
+#   pirate.deep_dup! :include => [:mateys, :treasures]
 #
 # ==== Cloning really deep
-#   pirate.clone! :include => {:treasures => :gold_pieces}
+#   pirate.deep_dup! :include => {:treasures => :gold_pieces}
 #
 # ==== Cloning really deep with multiple associations
-#   pirate.clone! :include => [:mateys, {:treasures => :gold_pieces}]
+#   pirate.deep_dup! :include => [:mateys, {:treasures => :gold_pieces}]
 #
 # 
 # Forked From at this: https://github.com/openminds/deep_cloning/
-# but it doesn't handle foreign_keys correctly
 #
-# preforms a recursive deep_copy / clone on an object, saving referenced models as the "stack" unfolds
-module DeepCloning
+# preforms a recursive deep dup
+
+module DeepDup
   # @param [Hash] options 
   # defaults = {:except => [:updated_at, :created_at, :id], 
   #            :include => []}
@@ -36,7 +37,7 @@ module DeepCloning
   #       even if a :exclude is passed through the formal parameter options
   #
   # @return [ActiveRecord::Base] the Object that was cloned
-  def clone!(options = {})
+  def deep_dup(options = {})
     defaults = {:except => [:updated_at, :created_at, :id], 
                 :include => []}
 
@@ -53,8 +54,8 @@ module DeepCloning
     # add current class to exclusions to prevent infinite loop
     exceptions << our_foreign_key
 
-    # doesn't save, only copies self's attributes
-    kopy = self.clone
+    # duplicate original object
+    kopy = self.dup
 
     if kopy.respond_to?("#{options[:previous_version_attr]}=")
       kopy.send("#{options[:previous_version_attr]}=", self)
@@ -64,9 +65,6 @@ module DeepCloning
       # attributes_from_column_definition is deprecated in rails > 2.3.8
       kopy[attribute] = attributes_from_column_definition[attribute.to_s]
     } if skip_attributes
-
-    # save before we need self's id for has_many / has_one relationships
-    kopy.save_with_validation(false)
 
     if options[:include]
       Array(options[:include]).each do |association, deep_associations|
@@ -82,12 +80,12 @@ module DeepCloning
         next if reflected_association.nil?
         cloned_object = case reflected_association.macro
                         when :belongs_to, :has_one
-                          ref_object = self.send(association).clone!(options)
+                          ref_object = self.send(association).deep_dup(options)
                           kopy.send("#{association}=", ref_object[:id])
                           ref_object
                         when :has_many, :has_and_belongs_to_many
                           self.send(association).collect { |obj| 
-                            ref_object = obj.clone!(options)
+                            ref_object = obj.deep_dup(options)
                             ref_object.send("#{our_foreign_key}=", kopy[:id]) if ref_object
                             ref_object
                           }
@@ -97,9 +95,8 @@ module DeepCloning
       end
     end
 
-    kopy.save_with_validation(false)
     return kopy
   end
 end
 require "active_record"
-ActiveRecord::Base.send(:include, DeepCloning)
+ActiveRecord::Base.send(:include, DeepDup)
